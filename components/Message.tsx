f47f9d5bbc20b84ff9865f99e229b4030d993d1c
Jie Chen <jiechenmc@gmail.com>
2022-11-20 12:49:53 -0500

    update styling

--- components/Message.tsx
+++ components/Message.tsx
@@ -2,8 +2,8 @@ export default function Message({ author, content, createdAt }: any) {
   const date = new Date(createdAt).toUTCString();
   if (content.startsWith("https"))
     return (
-      <div class="relative w-full h-20 bg-blue-200 m-4 p-4 overflow-scroll">
-        <img src={content} />
+      <div class="relative w-full h-full bg-blue-200 m-4 p-4 overflow-scroll">
+        <img src={content} width={300} height={300} />
         <div class="absolute right-20">
           <small>{author}</small>
         </div>
